5e9c3d6843f6a441539afb216fabdc10ba2a98f0
kasskata <kassskata@gmail.com>
2014-10-05 15:52:27 +0300

    Characters/Soldier added New Scripts for Camera, Weapon and Projectile.prefab

--- Assets(ALFA(NotAproved))/Script/PlayerDemo.cs
+++ Assets(ALFA(NotAproved))/Script/PlayerDemo.cs
@@ -8,6 +8,15 @@ public class PlayerDemo : MonoBehaviour
     private float speed = 10;
     private RaycastHit hit;
     Vector3 lookTarget;
+
+    public AnimationClip idle;
+    public AnimationClip run;
+    public AnimationClip fire;
+    public AnimationClip die;
+
+    //TODO:Health system
+    private float health = 65000;
+
     void Start()
     {
 
@@ -15,26 +24,57 @@ public class PlayerDemo : MonoBehaviour
 
     void Update()
     {
-        MoveWASD();
-        Rotation();
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        //Moving & Firing
+        if (horizontalMove != 0 || verticalMove != 0)
+        {
+            MoveWASD(horizontalMove, verticalMove);
+            Rotation();
+        }
+        // Idle
+        else
+        {
+            // Fire
+            if (Input.GetMouseButton(0))
+            {
+                //TODO: Mouse Button Shoot the enemy
+                animation.CrossFade(fire.name);
+            }
+            else
+            {
+                animation.CrossFade(idle.name);
+            }
+
+            Rotation();
+
+        }
+        //Dead
+        if (health <= 0)
+        {
+            animation.CrossFade(die.name);
+        }
+
     }
 
     private void Rotation()
     {
-         var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-         RaycastHit hit;
-    if (Physics.Raycast (ray, out hit)) {
-        lookTarget = hit.point;
-    }
- 
-    transform.LookAt(lookTarget);
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            lookTarget = hit.point;
+
+        }
+        transform.LookAt(lookTarget);
     }
 
-    public void MoveWASD()
+    public void MoveWASD(float horizontalMove, float verticalMove)
     {
-        float horizontalMove = Input.GetAxis("Horizontal");
-        float verticalMove = Input.GetAxis("Vertical");
+        transform.position += (Vector3.forward * (-horizontalMove) + Vector3.right * verticalMove) * Time.deltaTime * speed;
 
-        transform.position += (Vector3.forward * (-horizontalMove) + Vector3.right * verticalMove) * speed * Time.deltaTime;
+        animation.CrossFade(run.name);
     }
+
 }
