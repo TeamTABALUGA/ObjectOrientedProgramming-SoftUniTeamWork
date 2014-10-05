d9108e1f011d3cbd87354067520db2a8b22dde4b
kasskata <kassskata@gmail.com>
2014-10-05 22:12:23 +0300

    Complete Soldier's folder

---
+++ Assets(ALFA(NotAproved))/Script/Weapon/BulletProp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletProp : MonoBehaviour {
+    float timeDestroyBullet = 2;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Destroy(this.gameObject, timeDestroyBullet);
+
+	}
+}
