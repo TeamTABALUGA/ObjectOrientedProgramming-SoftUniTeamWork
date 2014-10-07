1dc2dd19b5b9bbed164ac2cedfa20a8b93dd26ba
Aleksandrov <emil.aleksandrov87@gmail.com>
2014-10-07 21:01:11 +0300

    Create GameStructure

---
+++ GameStructure/Testing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RPGGame.Weapons;
+namespace RPGGame
+{
+    class Testing
+    {
+
+        public static void Main()
+        {
+            Enemy bot = new Enemy(1f, new EnemyWeapon( WeaponType.Body, 23.2f, 1f ));
+            Player pesho = new Player(1f, new List<Weapon> { new PlayerWeapon(WeaponType.Knife, 50f, 10f)});
+            bot.EnemyWeapon.UseWeapon();
+            Console.WriteLine(bot.Health);
+            pesho.MakeDamage(bot);
+            
+            // Console.WriteLine(bot.Health); 
+            Console.WriteLine(bot.IsAlive());
+            foreach(PlayerWeapon weapon in pesho.PlayerWeapons)
+            {
+                Console.WriteLine( weapon.Name );
+            }
+        }
+
+    }
+}
---
+++ GameStructure/Weapons/EnemyWeapon.cs
@@ -0,0 +1,17 @@
+﻿namespace RPGGame.Weapons
+{
+    using System;
+
+    public class EnemyWeapon : Weapon, IFightable
+    {
+        public EnemyWeapon(WeaponType name, float damage, float range)
+            : base(name, damage, range)
+        {
+        }
+
+        public void UseWeapon()
+        {
+            Console.WriteLine("I am enemy. I use my body for weapon.");
+        }
+    }
+}
