1d275b8feac57cf52577d4e1bc93fb5ba94e449f
Jazastry <yogib@mail.bg>
2014-10-10 17:57:33 +0300

    Added EnemyScripts folder, updated FollowPlayer.cs script, enemy stops on touching .tag"Player".

--- Assets(ALFA(NotAproved))/Script/EnemyScripts/FollowPlayer.cs
+++ Assets(ALFA(NotAproved))/Script/EnemyScripts/FollowPlayer.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 using System.Collections;
 
 public class Follow1 : MonoBehaviour {
-
+	
 	Transform target;
 	Transform myTransform;
-
+	
 	// rotation and move speed can be overriden from the Unity UI
 	float moveSpeed = 3;
 	float rotationSpeed = 3;
-
+	
 	void Awake()
 	{
 		myTransform = transform;
 	}
-
+	
 	void Start () {
 		target = GameObject.FindGameObjectWithTag ("Player").transform;
 	}
@@ -25,5 +25,15 @@ public class Follow1 : MonoBehaviour {
 	void Update () {
 		transform.LookAt (new Vector3(target.position.x, transform.position.y, target.position.z));
 		myTransform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other) {
+		if ((other.tag == "Player")) {
+			moveSpeed = 0;
+		}
+	}
+	void OnTriggerExit(Collider other) {
+		if ((other.tag == "Player")) {
+			moveSpeed = 3;
+		}
 	}
 }
