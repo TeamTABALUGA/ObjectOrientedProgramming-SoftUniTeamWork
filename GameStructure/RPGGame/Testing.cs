1a865db6865b37d2f5e74cdd63d35a487a4ca199
Aleksandrov <emil.aleksandrov87@gmail.com>
2014-10-11 21:34:00 +0300

    Update GameStructure

--- GameStructure/RPGGame/Testing.cs
+++ GameStructure/RPGGame/Testing.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RPGGame.Creatures;
 using RPGGame.Places;
 using RPGGame.Weapons;
 namespace RPGGame
@@ -18,18 +19,18 @@ namespace RPGGame
             changeGunCharacteristics(70, weapons[0]);
             Console.WriteLine(weapons[0].Damage);
 
-            //Enemy bot = new Enemy(1f, weapons[0]);
-            //Player pesho = new Player(1f, new List<Weapon> { new PlayerWeapon(WeaponType.Knife, 50f, 10f)});
-            //bot.EnemyWeapon.UseWeapon();
-            //Console.WriteLine(bot.Health);
-            //pesho.MakeDamage(bot);
+            // Enemy bot = new Enemy(1f, weapons[0]);
+            // Player pesho = new Player(1f, new List<Weapon> { new PlayerWeapon(WeaponType.Knife, 50f, 10f)});
+            // bot.EnemyWeapon.UseWeapon();
+            // Console.WriteLine(bot.Health);
+            // pesho.MakeDamage(bot);
 
             //// Console.WriteLine(bot.Health); 
-            //Console.WriteLine(bot.IsAlive());
-            //foreach(PlayerWeapon weapon in pesho.PlayerWeapons)
-            //{
-            //    Console.WriteLine( weapon.Name );
-            //}
+            // Console.WriteLine(bot.IsAlive());
+            // foreach(PlayerWeapon weapon in pesho.PlayerWeapons)
+            // {
+            //     Console.WriteLine( weapon.Name );
+            // }
         }
 
         public static void changeGunCharacteristics(float damage, EnemyWeapon gun)
