344a1fbd772de44d2be439d466d2b88cdc986cb6
Aleksandrov <emil.aleksandrov87@gmail.com>
2014-10-11 22:15:30 +0300

    Update GameStructure

---
+++ GameStructure/RPGGame/Characteristics/HealthSystem.cs
@@ -0,0 +1,33 @@
+﻿namespace RPGGame.Characteristics
+{
+    using RPGGame.Interfaces;
+    public abstract class HealthSystem : Experience, IHealth
+    {
+        public const float START_HEALTH = 100;
+        public const float DEAD = 0;
+        private float currentHealth;
+
+        public float CurrentHealth { get; set; }
+
+        public bool IsAlive(IHealth creature)
+        {
+            bool isAlive = creature.CurrentHealth > 0;
+            if(isAlive)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public void DecreaseHealth(IHealth creature, Weapon weapon)
+        {
+            float decreasedHealth = creature.CurrentHealth - weapon.Damage;
+            creature.CurrentHealth = decreasedHealth;
+        }
+
+    }
+}
---
+++ GameStructure/RPGGame/Creatures/EnemyDifficulty.cs
@@ -0,0 +1,9 @@
+﻿namespace RPGGame.Creatures
+{
+    public enum EnemyDifficulty
+    {
+        Easy = 100, // Change the numbers if you don't like them !
+        Medium = 200,
+        Hard = 300
+    }
+}
