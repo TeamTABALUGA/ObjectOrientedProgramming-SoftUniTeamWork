f9aed80c9cb813f61b01546abaf4258502720d6a
Jazastry <yogib@mail.bg>
2014-10-21 13:58:31 +0300

    Added latest GatesScripts.

---
+++ Assets(ALFA(NotAproved))/Script/GatesScripts/PLayerGateB.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PLayerGateB : PlayerGate {
+
+	public GameObject gateB;
+	
+	// Use this for initialization
+	void Start () {
+		this.InitialState = false;
+		this.ChangedState = true;
+		this.IsBGate = true;
+		this.Gate = gateB;
+	}
+}
